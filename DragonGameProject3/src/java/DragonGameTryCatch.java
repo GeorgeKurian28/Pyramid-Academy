236ab5a25848ffdb7903f3a667cebce5afa8a8c0
George Kurian <32146010+GeorgeKurian28@users.noreply.github.com>
2022-07-04 14:53:38 -0500

    Updated with comments

--- DragonGameProject3/src/java/DragonGameTryCatch.java
+++ DragonGameProject3/src/java/DragonGameTryCatch.java
@@ -5,12 +5,24 @@ public class DragonGameTryCatch {
     private boolean dragon2;
     Scanner scan = new Scanner(System.in);
 
+    /**
+     * Generates the two dragons and randomly assigns one to be friendly and the other as hungry dragon
+     *
+     * @param - dragon1    -Boolean value for friendly dragon
+     * @param - dragon2    -Boolean value for friendly dragon
+     * @return - String    -friendly dragon
+     */
     public String getFriendlyDragon(){
         dragon1 = Math.floor(Math.random()*10) > 5 ?false: true;
         dragon2 = !dragon1;
         return dragon1 ? "dragon1" : "dragon2";
     }
 
+    /**
+     * The actual game. The user has to choose between two caves 1 and 2, any other option is returned as invalid
+     *
+     * @param - cave     -Integer value for the cave choosen by the user
+     */
 
     public void game(){
         String cave;
@@ -23,9 +35,16 @@ public class DragonGameTryCatch {
             System.out.println("\n A large dragon jumps in front of you! He opens his jaws and ...\nGobbles you down in one bite..");
     }
 
+    /**
+     * Launch the game and give the player option to continue and validate the option using a try catch block
+     *
+     * @param - option       -String the option choosen by the user to continue
+     * @param - valid        -Boolean to continue the game
+     */
     public void playGame(){
         String option;
         boolean valid = false;
+        game();
         while(!valid){
             System.out.println("Would you like to continue \n Enter Yes or No");
             option = scan.next();
@@ -42,6 +61,12 @@ public class DragonGameTryCatch {
         }
     }
 
+    /**
+     * Validate the input and throw exception if not valid
+     *
+     * @param - option       -String input for choosing the caves yes or no
+     * @return - the valid option
+     */
     public String getOption(String option)throws Exception{
         if(!(option.contains("Y") || option.contains("N") || option.contains("y") || option.contains("n"))) {
             throw new Exception("\n\n\n\n\n\n\n\n\nThis is not a valid input, please retry ");
@@ -50,6 +75,14 @@ public class DragonGameTryCatch {
             return option;
     }
 
+    /**
+     * Gets the user guess and validates it
+     *
+     * @param - cave         -String has to be 1 or 2
+     * @param - option       -Integer value of the correct option 1 or 2
+     * @param - valid        -Boolean to validate the input option
+     * @return - Welcome message
+     */
     public int getUserGuess(){
         String cave = "";
         int option = 0;
@@ -69,10 +102,13 @@ public class DragonGameTryCatch {
         }
         return option;
     }
-
+    /**
+     * Launches the application calls the playGame method to start the game
+     *
+     *
+     */
     public static void main(String ... args){
         DragonGameTryCatch dragonGameJUnitTesting = new DragonGameTryCatch();
-        dragonGameJUnitTesting.game();
         dragonGameJUnitTesting.playGame();
     }
 
