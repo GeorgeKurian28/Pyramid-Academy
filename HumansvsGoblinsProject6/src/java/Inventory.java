87a7423712505ac9e4f3cf8ab5b7e091c6c59733
George Kurian <32146010+GeorgeKurian28@users.noreply.github.com>
2022-07-17 00:18:43 -0500

    Comments added to different classes and Inventory Unit Test added
    Comments were added and minor corrections made to various classes and Unit test added to Inventory

--- HumansvsGoblinsProject6/src/java/Inventory.java
+++ HumansvsGoblinsProject6/src/java/Inventory.java
@@ -2,42 +2,93 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Inventory {
+    /******
+     * The Goblin Object has these attributes and methods,
+     *
+     * @param -health    -Integer value that stores the health in the inventory
+     * @param -weapons   -HashMap with the name of the weapon as the key and the strength of the weapon as an integer
+     * @param -location  -Integer array that stores the x and y coordinate of the position in the Grid
+     */
     private int health;
-    //private ArrayList<Weapon> weapons = new ArrayList<>();
-
     private HashMap<String, Integer> weapons = new HashMap<>();
     private int[] location;
 
-    public Inventory(int positionX, int positionY){
-        health = 50;
+    /*****
+     * The four argument constructor for the Inventory object
+     *
+     */
+    public Inventory(int positionX, int positionY,int health, String ...arr){
+        this.health = health;
         setLocation(positionX,positionY);
-        weapons.put("Gun",20);
-        weapons.put("Grenade",40);
-        //weapons.add(new Weapon("Gun",20));
-        //weapons.add(new Weapon("Grenade", 10));
+        for(int  i = 0 ; i < arr.length; i += 2)
+            this. weapons.put(arr[i],Integer.parseInt(arr[i+1]));
     }
 
+    /*****
+     * The three argument constructor for the Inventory object
+     *
+     * This calls the constructor with four arguments
+     */
+    public Inventory(int positionX, int positionY, String ...arr){
+        this(positionX,positionY,50,arr);
+    }
+
+    /*****
+     * The two argument constructor for the Inventory object
+     *
+     * This calls the constructor with three arguments
+     */
+    public Inventory(int positionX, int positionY){
+        this(positionX,positionY,"Gun","20","Grenade","40");
+    }
+
+    /*****
+     * The getHealth Method returns the health in the inventory in the cell
+     *
+     * @return returns the int variable health
+     */
     public int getHealth() {
         return health;
     }
 
-    public void setQuantity(int health) {
-        this.health = health;
-    }
-
+    /*****
+     * The getLocation method return the location of the inventory
+     *
+     * @return returns the location which is an integer array
+     */
     public int[] getLocation() {
         return location;
     }
 
+    /**************
+     * The setlocation method sets the location of the inventory
+     *
+     * @param positionX -Integer value that contains the x value of the position
+     * @param positionY -Integer value that contains the y value of the position
+     */
     public void setLocation(int positionX, int positionY) {
         int[] pos = {positionX,positionY};
         this.location = pos;
     }
 
+    /**************
+     * The setWeapons method adds a new weapon to the stack and if the weapon is already present increases updates its strength
+     *
+     * @param weapon
+     * @param strength
+     */
     public void setWeapons(String weapon, int strength ){
-        weapons.put(weapon,strength);
+        if(weapons.containsKey(weapon))
+            weapons.put(weapon,weapons.get(weapon)+strength);
+        else
+            weapons.put(weapon,strength);
     }
 
+    /***************
+     * The getWeapons method, returns a HashMap of the wepons in the location
+     *
+     * @return returns a HashMap of weapons
+     */
     public HashMap<String, Integer> getWeapons(){
         return weapons;
     }
--- HumansvsGoblinsProject6/src/test/InventoryTest.java
+++ HumansvsGoblinsProject6/src/test/InventoryTest.java
@@ -5,36 +5,52 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryTest {
+    Inventory inventory1;
+    Inventory inventory2;
+    Inventory inventory3;
 
     @BeforeEach
     void setUp() {
-    }
-
-    @AfterEach
-    void tearDown() {
+        inventory1 = new Inventory(2,3);
+        inventory2 = new Inventory(5,7,"Gun","30");
+        inventory3 = new Inventory(9,6,100,"Grenade","70");
     }
 
     @Test
     void getHealth() {
-    }
-
-    @Test
-    void setQuantity() {
+        assertEquals(50,inventory1.getHealth());
+        assertEquals(100,inventory3.getHealth());
     }
 
     @Test
     void getLocation() {
+        assertEquals(2,inventory1.getLocation()[0]);
+        assertEquals(3, inventory1.getLocation()[1]);
+        assertEquals(5,inventory2.getLocation()[0]);
+        assertEquals(7, inventory2.getLocation()[1]);
     }
 
     @Test
     void setLocation() {
-    }
-
-    @Test
-    void setWeapons() {
+        inventory1.setLocation(4,5);
+        assertEquals(4,inventory1.getLocation()[0]);
+        assertEquals(5, inventory1.getLocation()[1]);
     }
 
     @Test
     void getWeapons() {
+        assertEquals("[Gun, Grenade]", inventory1.getWeapons().keySet().toString());
+        assertEquals("[Gun]", inventory2.getWeapons().keySet().toString());
+        assertEquals("[Grenade]", inventory3.getWeapons().keySet().toString());
+        assertEquals(20, inventory1.getWeapons().get("Gun"));
+        inventory1.setWeapons("Gun",10);
+        assertEquals(30, inventory1.getWeapons().get("Gun"));
+        inventory3.setWeapons("Gun",10);
+        assertEquals("[Gun, Grenade]", inventory3.getWeapons().keySet().toString());
+
+    }
+
+    @AfterEach
+    void tearDown() {
     }
 }
